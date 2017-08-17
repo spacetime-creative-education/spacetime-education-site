fc95533e2bc392e8736a8c7478473c0e7df3986c
neotheicebird <neotheicebird@gmail.com>
2017-08-17 17:39:28 +0530

    work in progress

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Headroom from 'react-headroom';
+import LazyLoad from 'react-lazyload';
+import YouTube from 'react-youtube';
+import MyMap from './MyMap.js';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <LazyLoad height={200}>
+          <MyMap />
+        </LazyLoad>
+        <LazyLoad height={200}>
+          <YouTube />
+        </LazyLoad>
       </div>
     );
   }
