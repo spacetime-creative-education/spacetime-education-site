79b9bfd2b5f825bfb4d570958fec1ca79e3ed6a3
neotheicebird <neotheicebird@gmail.com>
2017-09-09 14:31:49 +0530

    acme challenge in public path

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ const App = () => (
   <Router>
     <div>
       <Route exact path="/" component={Home} />
-    <Route path="/.well-known/acme-challenge/:hash" render={() => <div>X4VZlDFr1sEtwZEDlbKYqLzwEPpXEhnIBwPSPG9zkCM.BB95YhjjV0rrfAxwNiQ4NL6zfZolmxs3BbsUpGkbANo</div>} />
     </div>
   </Router>
 );
