83ceb51a35ba9c952cba59c3e30659b793072452
neotheicebird <neotheicebird@gmail.com>
2017-09-13 16:18:45 +0530

    changing teacherpreneurship to edstartup

--- src/AboutUs.js
+++ src/AboutUs.js
@@ -10,7 +10,7 @@ class AboutUs extends Component {
     return (
       <div className="aboutus">
         <div className="about-heading"><h2>About Us</h2></div>
-      <div className="about-desc"><p>Spacetime Education is a newborn teacherpreneurship. We want education to be fun, not see it as just a tool for jobhunting, but more as a platform to play and explore the immense creative treasures in all of us.
+      <div className="about-desc"><p>Spacetime Education is a newborn EdStartup. We want education to be fun, not see it as just a tool for jobhunting, but more as a platform to play and explore the immense creative treasures in all of us.
         <br/><br/>There is explosive creativity happening around the world. Things are changing rapidly and for the first time in human history we are on the brink of a future that is going to go into a warp drive and no one knows whats on the other side. Tech Startups and Innovative businesses are popping up everywhere. We want to take part in this change. We want to serve people to get better equipped to shape humanity's future. Its all so exciting!</p></div>
       </div>
     )
